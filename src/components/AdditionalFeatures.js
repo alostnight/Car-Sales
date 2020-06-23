aa85bc44a6180188edcdce470567716d947ed8a7
Jose Paquian <alostnight1993@outlook.com>
2020-06-23 17:43:18 -0500

    Done

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import AdditionalFeature from './AdditionalFeature';
+import React from "react";
+import AdditionalFeature from "./AdditionalFeature";
+import { connect } from "react-redux";
 
-const AdditionalFeatures = props => {
+const AdditionalFeatures = (props) => {
   return (
     <div className="content">
+      <h2>Buy extra items here:</h2>
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {props.additionalFeatures.map((item) => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
-        <p>Nice looking car!</p>
+        <p>Thank You for Adding All Items!</p>
       )}
     </div>
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = (state) => {
+  return {
+    additionalFeatures: state.additionalFeatures,
+  };
+};
+
+export default connect(mapStateToProps, {})(AdditionalFeatures);
